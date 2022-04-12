61e86dc1c3e106317926cbc1770ea07cbe4fdd45
Sebastian Barney <sebastianbarney1@gmail.com>
2022-04-11 18:37:02 -0600

    Weird Equations added for thing

--- MATH2250_FinalProject.m
+++ MATH2250_FinalProject.m
@@ -1,3 +1,19 @@
+x_1850i = [11e2, 2.1e3, 7e2, 3.8e4, 4.8e7]
+
+x_1850 = x_1850i'
+
+a= eye(5,5)*1850
 
 
-%%
+
+
+A*x_1850
+
+tint = 2020
+
+tfin = 2020
+
+tao = linspace((tint - tint), (tfin - tint), 5)
+A = diag(tao)
+
+A*x_1850
