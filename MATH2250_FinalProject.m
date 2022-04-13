bf3eb81cd82f04ab240a9d84cf55eda94c208a97
Sebastian Barney <sebastianbarney1@gmail.com>
2022-04-13 17:13:29 -0600

    Deleted and Changed stuff

--- MATH2250_FinalProject.m
+++ MATH2250_FinalProject.m
@@ -5,24 +5,24 @@ A = [0 , 120, 90.15, 0, 0; 120, 0, 0, 0, 0; 9, 0, 0, 5.3, 0; 0, 0, 5, 0, 0.3; 0.
 [V,D] = eig(A)
 
 tint = 1850
+
+
 v1 = V(:,1)
 v2 = V(:,2)
 v3 = V(:,3)
 v4 = V(:,4)
 v5 = V(:,5)
+eta = [v1,v2,v3,v4,v5]
+Lambda = [D(1,1), D(2,2), D(3,3), D(4,4),  D(5,5)]
+c = [c1,c2,c3,c4,c5]  
 
-Lambda1 = D(1,1)
-Lambda2 = D(2,2)
-Lambda3 = D(3,3)
-Lambda4 = D(4,4)
-Lambda5 = D(5,5)
+solve(eqn,x)
 
-c2
-c3
-c4
-c5
-(c1)*(v1) == ((x_1850i(1))/exp(Lambda1*tint))
-(c2)*(v2) == ((x_1850i(2))/exp(Lambda2*tint))
-(c3)*(v3) == ((x_1850i(3))/exp(Lambda3*tint))
-(c4)*(v4) == ((x_1850i(4))/exp(Lambda4*tint))
-(c5)*(v5) == ((x_1850i(5))/exp(Lambda5*tint))
+% x_1850i(1) == eta(1)*exp(Lambda(1)*tint)*c(1)
+
+
+% x_1850i(n) == eta(n)*exp(Lambda(n)*tint)*c(n)
+
+% for n = 1:5
+% a = vpasolve(x_1850i(n) == eta(n)*exp(Lambda(n)*tint)*c(n),c(n))
+% end
