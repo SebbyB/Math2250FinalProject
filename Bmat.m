6439e3db5b181ca35cd50d2e7fc7fbb3fe53576b
Sebastian Barney <sebastianbarney1@gmail.com>
2022-04-18 16:04:32 -0600

    Bmat Created For Q2

---
+++ Bmat.m
@@ -0,0 +1,28 @@
+clc
+clear
+format long
+x_1850i = [600 2100 700]
+B = [-210.15/600 , 13/70, 0; ...
+    1/5, -120/2100, 0; ...
+    90/600 , 0, -95.3/700]
+
+[V,D] = eig(B);
+
+t = 0;
+a = V\x_1850i';
+f = @(t) a.*exp(diag(D)*t);
+x = @(t) V*f(t);
+t = linspace(0,50);
+
+X = x(t);
+% plot(t,X(1:3,:))
+hold on
+% plot(2.2222,851.056,'r*')
+plot(t,X(1,:),'r')
+plot(t,X(2,:),'g')
+plot(t,X(3,:),'b')
+title('Emissions over Time with immediate drop to zero - Simplified')
+ylabel('Carbon Emissions (GigaTonnes)')
+xlabel('Time (Years)')
+Labels = ["Atmosphere" "BioSphere" "Surface Ocean"]
+legend(Labels);
