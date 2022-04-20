73ab0661ec12aa40b6e8f6c10e4ef96d0eb877fb
BDavis44 <103466057+BDavis44@users.noreply.github.com>
2022-04-20 17:37:22 -0600

    Right Matrix

--- SimplifiedCarbonModel.m
+++ SimplifiedCarbonModel.m
@@ -1,10 +1,10 @@
 clc
 clear
 format long
-x_1850i = [600 2100 700]
-B = [-210.15/600 , 13/70, 0; ...
-    1/5, -120/2100, 0; ...
-    90/600 , 0, -95.3/700]
+x_1850i = [1100 2100 700]
+B = [-210/600 , 120/2100, 90/700; ...
+    120/600, -120/2100, 0; ...
+    90/600 , 0, -90/700];
 
 [V,D] = eig(B);
 
@@ -24,5 +24,5 @@ plot(t,X(3,:),'b')
 title('Emissions over Time with immediate drop to zero - Simplified')
 ylabel('Carbon Emissions (GigaTonnes)')
 xlabel('Time (Years)')
-Labels = ["Atmosphere" "BioSphere" "Surface Ocean"]
+Labels = ["Atmosphere" "BioSphere" "Surface Ocean"];
 legend(Labels);
